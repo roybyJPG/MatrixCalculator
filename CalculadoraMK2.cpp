07aff753cd697f5569704147784648bc77187c7f
Juan Pablo Galicia <galicia.juanpablo202gmail.com>
2022-10-29 21:31:04 -0500

    Parte aleatoria en funciones independientes por opercacion, con motores y semillas independientes

--- CalculadoraMK2.cpp
+++ CalculadoraMK2.cpp
@@ -3,23 +3,30 @@
 #include <chrono>
 #include <string>
 #include <sstream>
+#include <iomanip>
 #include <stdlib.h>
+
 using namespace std;
 
+void menu();
 void leerMatriz(int **M, int fil, int col);
+void imprimirMatriz(int **M, int fil, int col);
 void sumaMatrices();
 void restaMatrices();
 void multiplicacionMatrices();
 void matrizTranspuesta();
-void imprimirMatriz(int **M, int fil, int col);
-void menu();
 
-void matrizAleatoria( int fil, int col );
+void sumaMatriz_Aleatoria(int fil, int col);
+void restaMatriz_Aleatoria(int fil, int col);
+void byMatriz_Aleatoria(int fil, int col);
+void transMatriz_Aleatoria(int fil, int col);
 
-//void leerMatrizAleatoria(int **M, int filRan, int columRan);
+
+//=======================================================================//
 
 void menu()
 {
+
     cout << "\n         CALCULADORA DE MATRICES"
          << "\n============================================"
          << "\n1.- Suma de Matrices ................... (1)"
@@ -38,14 +45,14 @@ int main()
 
     do{
         menu();
-        cin >> opcion;
-        if( cin.fail() || cin.bad() ){
+        cin>>opcion;
+        if(cin.fail() || cin.bad()){
             opcion = 0;
             cin.clear();
-            cin.ignore( '\n', 80 );
+            cin.ignore('\n', 80);
         }
 
-            switch( opcion ){
+            switch(opcion){
                 case 1:
                     sumaMatrices();
                     break;
@@ -63,25 +70,24 @@ int main()
                     break;
 
                 case 5:
-                    cout << "\n\nFIN DEL PROGRAMA\n\n";
+                    cout<<"\n\nFIN DEL PROGRAMA\n\n";
                     exit(0);
                     break;
 
                 default:
-                    cout << "\n\nOPCION NO VALIDA\n\n";
+                    cout<<"\n\nOPCION NO VALIDA\n\n";
                     break;
         }
-
-    }while( opcion != 5 );
+    }while(opcion != 5);
 }
 
 void leerMatriz(int **M, int fil, int col)
 {
-        cout << "\nRellenar la matriz:\n";
-        for( int i = 0; i < fil; i++ ){
-            for( int j = 0; j < col; j++ ){
-                cout << "Matriz[" << i << "][" << j << "]: ";
-                cin >> M[i][j];
+        cout<<"\nRellenar la matriz:\n";
+        for(int i = 0; i < fil; i++){
+            for(int j = 0; j < col; j++){
+                cout<<"Matriz["<<i<<"]["<<j<<"]: ";
+                cin>>M[i][j];
             }
         }
 }
@@ -90,44 +96,47 @@ void sumaMatrices()
 {
     int fil, col;
 
-    cout << "\nLAS MATRICES DEBEN TENER LA MISMA DIMENSION\n"
-         << "\nDimension de la matriz:";
-    cout << "\nFilas de la matriz: "; cin >> fil;
-    cout << "Columnas de la matriz: "; cin >> col;
+    cout<<"\nLAS MATRICES DEBEN TENER LA MISMA DIMENSION\n"
+        <<"\nDimension de la matriz:";
+    cout<<"\nFilas de la matriz: ";
+    cin>>fil;
+    cout<<"Columnas de la matriz: ";
+    cin>>col;
+    cout<<endl;
 
     if(fil<=4,col<=4){
 
         int** A = new int*[fil];
-        for( int i = 0; i < fil; i++ )
+        for(int i=0; i<fil; i++)
             A[i] = new int[col];
 
         int** B = new int*[fil];
-        for( int i = 0; i < fil; i++ )
+        for(int i=0; i<fil; i++)
             B[i] = new int[col];
 
         int** C = new int*[fil];
-        for( int i = 0; i < fil; i++ )
+        for(int i=0; i<fil; i++)
             C[i] = new int[col];
 
-        cout << "\nDatos de la matriz A: ";
-        leerMatriz( A, fil, col );
+        cout<<"\nDatos de la matriz A: ";
+        leerMatriz(A, fil, col);
 
-        cout << "\nDatos de la matriz B: ";
-        leerMatriz( B, fil, col );
+        cout<<"\nDatos de la matriz B: ";
+        leerMatriz(B, fil, col);
 
-        for( int i = 0; i < fil; i++ )
-            for( int j = 0; j < col; j++ )
+        for(int i=0; i<fil; i++)
+            for(int j=0; j<col; j++)
                 C[i][j] = A[i][j] + B[i][j];
 
-        cout << "\nMatriz A:\n";
-        imprimirMatriz( A, fil, col );
-        cout << "\nMatriz B:\n";
-        imprimirMatriz( B, fil, col );
-        cout << "\nSuma de las matrices (A+B):\n";
-        imprimirMatriz( C, fil, col );
+        cout<<"\nMatriz A:\n";
+        imprimirMatriz(A, fil, col);
+        cout<<"\nMatriz B:\n";
+        imprimirMatriz(B, fil, col);
+        cout<<"\nSuma de las matrices (A+B):\n";
+        imprimirMatriz(C, fil, col);
 
     } else{
-        matrizAleatoria(fil,col);
+        sumaMatriz_Aleatoria(fil, col);
     }
 }
 
@@ -135,92 +144,105 @@ void restaMatrices()
 {
     int fil, col;
 
-    cout << "\nLAS MATRICES DEBEN TENER LA MISMA DIMENSION\n"
-         << "\nDimension de la matriz:";
-    cout << "\nFilas de la matriz: "; cin >> fil;
-    cout << "Columnas de la matriz: "; cin >> col;
+    cout<<"\nLAS MATRICES DEBEN TENER LA MISMA DIMENSION\n"
+        <<"\nDimension de la matriz:";
+    cout<<"\nFilas de la matriz: ";
+    cin>>fil;
+    cout<<"Columnas de la matriz: ";
+    cin>>col;
+    cout<<endl;
 
-    int** A = new int*[fil];
-    for( int i = 0; i < fil; i++ )
-        A[i] = new int[col];
+    if(fil<=4,col<=4){
 
-    int** B = new int*[fil];
-    for( int i = 0; i < fil; i++ )
-        B[i] = new int[col];
+        int** A = new int*[fil];
+        for(int i=0; i<fil; i++)
+            A[i] = new int[col];
 
-    int** C = new int*[fil];
-    for( int i = 0; i < fil; i++ )
-        C[i] = new int[col];
+        int** B = new int*[fil];
+        for(int i=0; i<fil; i++)
+            B[i] = new int[col];
 
-    cout << "\nDatos de la matriz A: ";
-    leerMatriz( A, fil, col );
+        int** C = new int*[fil];
+        for(int i=0; i<fil; i++)
+            C[i] = new int[col];
 
-    cout << "\nDatos de la matriz B: ";
-    leerMatriz( B, fil, col );
+        cout<<"\nDatos de la matriz A: ";
+        leerMatriz(A, fil, col);
 
-    for( int i = 0; i < fil; i++ )
-        for( int j = 0; j < col; j++ )
-            C[i][j] = A[i][j] - B[i][j];
+        cout<<"\nDatos de la matriz B: ";
+        leerMatriz(B, fil, col);
 
-    cout << "\nMatriz A:\n";
-    imprimirMatriz( A, fil, col );
-    cout << "\nMatriz B:\n";
-    imprimirMatriz( B, fil, col );
-    cout << "\nResta de las matrices (A-B):\n";
-    imprimirMatriz( C, fil, col );
+        for(int i=0; i<fil; i++)
+            for(int j=0; j<col; j++)
+                C[i][j] = A[i][j] - B[i][j];
+
+        cout<<"\nMatriz A:\n";
+        imprimirMatriz(A, fil, col);
+        cout<<"\nMatriz B:\n";
+        imprimirMatriz(B, fil, col);
+        cout<<"\nResta de las matrices (A-B):\n";
+        imprimirMatriz(C, fil, col);
+
+    } else{
+        restaMatriz_Aleatoria(fil, col);
+    }
 }
 
 void multiplicacionMatrices()
 {
     int filA, colA, filB, colB;
 
-    cout << "\n    EL NUMERO DE COLUMNAS DE LA MATRIZ A"
-         << "\nDEBE COINCIDIR CON EL DE FILAS DE LA MATRIZ B\n";
+    cout<<"\n    EL NUMERO DE COLUMNAS DE LA MATRIZ A"
+        <<"\nDEBE COINCIDIR CON EL DE FILAS DE LA MATRIZ B\n";
 
-    cout << "\nDimension de la matriz A:";
-    cout << "\nFilas de la matriz: "; cin >> filA;
-    cout << "Columnas de la matriz: "; cin >> colA;
+    cout<<"\nDimension de la matriz A:";
+    cout<<"\nFilas de la matriz: ";
+    cin>>filA;
+    cout<<"Columnas de la matriz: ";
+    cin>>colA;
 
     int** A = new int*[filA];
-    for( int i = 0; i < filA; i++ )
+    for(int i=0; i<filA; i++)
         A[i] = new int[colA];
 
-    leerMatriz( A, filA, colA );
+    leerMatriz(A, filA, colA);
 
-    cout << "\nDimension de la matriz B:";
-    cout << "\nFilas de la matriz: "; cin >> filB;
-    cout << "Columnas de la matriz: "; cin >> colB;
+    cout<<"\nDimension de la matriz B:";
+    cout<<"\nFilas de la matriz: ";
+    cin>>filB;
+    cout<<"Columnas de la matriz: ";
+    cin>>colB;
 
     int** B = new int*[filB];
-    for( int i = 0; i < filB; i++ )
+    for(int i=0; i<filB; i++)
         B[i] = new int[colB];
 
-    leerMatriz( B, filB, colB );
+    leerMatriz(B, filB, colB);
 
     int** C = new int*[filA];
-    for( int i = 0; i < filA; i++ )
+    for(int i=0; i<filA; i++)
         C[i] = new int[colA];
 
-    if( colA == filB ){
-        for( int i = 0; i < filA; ++i ){
-            for( int j = 0; j < colB; ++j ){
+    if(colA == filB){
+        for(int i=0; i<filA; ++i){
+            for(int j=0; j<colB; ++j){
                 C[i][j] = 0;
-                for( int z = 0; z < colA; ++z )
+                for(int z=0; z<colA; ++z)
                     C[i][j] += A[i][z] * B[z][j];
             }
         }
 
-        cout << "\nMatriz A:\n";
-        imprimirMatriz( A, filA, colA );
-        cout << "\nMatriz B:\n";
-        imprimirMatriz( B, filB, colB );
-        cout << "\nMultiplicacion de las matrices (A*B):\n";
-        imprimirMatriz( C, filA, colB );
+        cout<<"\nMatriz A:\n";
+        imprimirMatriz(A, filA, colA);
+        cout<<"\nMatriz B:\n";
+        imprimirMatriz(B, filB, colB);
+        cout<<"\nMultiplicacion de las matrices (A*B):\n";
+        imprimirMatriz(C, filA, colB);
 
     }else
-        cout << "\n         NO SE PUEDEN MULTIPLICAR"
-             << "\n    EL NUMERO DE COLUMNAS DE LA MATRIZ A"
-             << "\nDEBE COINCIDIR CON EL DE FILAS DE LA MATRI B";
+        cout<<"\n         NO SE PUEDEN MULTIPLICAR"
+            <<"\n    EL NUMERO DE COLUMNAS DE LA MATRIZ A"
+            <<"\nDEBE COINCIDIR CON EL DE FILAS DE LA MATRI B";
 
 }
 
@@ -228,74 +250,181 @@ void matrizTranspuesta()
 {
     int filO, colO;
 
-    cout << "\nLAS MATRICES DEBEN TENER LA MISMA DIMENSION\n"
-         << "\nDimension de la matriz:";
-    cout << "\nFilas de la matriz: "; cin >> filO;
-    cout << "Columnas de la matriz: "; cin >> colO;
+    cout<<"\nLAS MATRICES DEBEN TENER LA MISMA DIMENSION\n"
+        <<"\nDimension de la matriz:";
+    cout<<"\nFilas de la matriz: ";
+    cin>>filO;
+    cout<<"Columnas de la matriz: ";
+    cin>>colO;
+    cout<<endl;
 
     if(filO<=4,colO<=4){
 
     int** A = new int*[filO];
-        for( int i = 0; i < filO; i++ )
+        for(int i = 0; i < filO; i++)
             A[i] = new int[colO];
 
-    cout << "\nDatos de la matriz A: ";
-    leerMatriz( A, filO, colO );
+    cout<<"\nDatos de la matriz A: ";
+    leerMatriz(A, filO, colO);
 
-    cout << "\nMatriz original:\n";
-    imprimirMatriz( A, filO, colO );
+    cout<<"\nMatriz original:\n";
+    imprimirMatriz(A, filO, colO);
 
-    cout << "\nMatriz transpuesta:\n";
-    for( int i = 0; i < filO; i++ ){
-        cout << "\n| ";
-        for( int j = 0; j < colO; j++ )
-            cout << A[j][i] << " ";
+    cout<<"\nMatriz transpuesta:\n";
+    for(int i = 0; i < filO; i++){
+        cout<<"\n| ";
+        for(int j = 0; j < colO; j++)
+            cout<<A[j][i]<<" ";
 
-        cout << " |";
+        cout<<" |";
     }
     cout << endl;
 
     } else{
-        matrizAleatoria(filO, colO);
+        transMatriz_Aleatoria(filO, colO);
     }
-
 }
 
-void matrizAleatoria( int fil, int col )
+void sumaMatriz_Aleatoria(int fil, int col)
 {
-    int M[fil][col];
     unsigned int seedMatrix=chrono::steady_clock::now().time_since_epoch().count();
     default_random_engine motor(seedMatrix);
-    uniform_int_distribution<int>filRan(0,99);
-    uniform_int_distribution<int>colRan(0,99);
+    uniform_int_distribution<int>sumRanA(0,99);
+    uniform_int_distribution<int>sumRanB(0,99);
 
-    //for( int i; i < fil; i++ ){
-        //M[i]=filRan(motor);
-        //cout << "\n| ";
-            //for( int j; j < col; j++ ){
-           // M[j]=colRan(motor);
-            //cout << M[i][j] << " ";
-
-    //
-
-    //cout<<"Columnas: "<<columRan(motor)<<endl;
-    //cout<<"Filas: "<<filRan(motor)<<endl;
-
-}
-
-void leerMatrizAleatoria(int M, int fil, int col)
-{
-
-}
-
-void imprimirMatriz( int **M, int fil, int col )
-{
-    for( int i = 0; i < fil; i++ ){
-        cout << "\n| ";
-        for( int j = 0; j < col; j++ )
-            cout << M[i][j] << " ";
-
-        cout << " |";
+    int MAsumA[fil][col], MAsumB[fil][col];
+    //===========================================
+    cout<<"Matriz aleatoria A: "<<endl;
+    for(int i=0; i<fil; i++){
+        for(int j=0; j < col; j++){
+        MAsumA[i][j]=sumRanA(motor);
+        }
     }
-    cout << endl;
+    for(int i=0; i<fil; ++i){
+        cout<<"\n| ";
+        for(int j=0; j<col; ++j)
+            cout<<setw(3)<<MAsumA[i][j]<<" ";
+        cout<<" |";
+    }
+    cout<<"\n"<<endl;
+    //===========================================
+    cout<<"Matriz aleatoria B: "<<endl;
+    for(int i=0; i<fil; i++){
+        for(int j=0; j < col; j++){
+        MAsumB[i][j]=sumRanB(motor);
+        }
+    }
+    for(int i=0; i<fil; ++i){
+        cout<<"\n| ";
+        for(int j=0; j<col; ++j)
+            cout<<setw(3)<<MAsumB[i][j]<<" ";
+        cout<<" |";
+    }
+    cout<<endl;
+    //===========================================
+    int** MAsumC = new int*[fil];
+        for(int i=0; i<fil; i++)
+            MAsumC[i] = new int[col];
+    cout<<"\nSuma de las matrices aleatorias (A+B):\n";
+    for(int i=0; i<fil; i++)
+            for(int j=0; j<col; j++)
+                MAsumC[i][j] = MAsumA[i][j] + MAsumB[i][j];
+    //===========================================
+    imprimirMatriz(MAsumC, fil, col);
+}
+
+void restaMatriz_Aleatoria(int fil, int col)
+{
+    unsigned int seedMatrix2=chrono::steady_clock::now().time_since_epoch().count();
+    default_random_engine motor2(seedMatrix2);
+    uniform_int_distribution<int>restRanA(0,99);
+    uniform_int_distribution<int>restRanB(0,99);
+
+    int MArestA[fil][col], MArestB[fil][col];
+    //===========================================
+    cout<<"Matriz aleatoria A: "<<endl;
+    for(int i=0; i<fil; i++){
+        for(int j=0; j < col; j++){
+        MArestA[i][j]=restRanA(motor2);
+        }
+    }
+    for(int i=0; i<fil; ++i){
+        cout<<"\n| ";
+        for(int j=0; j<col; ++j)
+            cout<<setw(3)<<MArestA[i][j]<<" ";
+        cout<<" |";
+    }
+    cout<<"\n"<<endl;
+    //===========================================
+    cout<<"Matriz aleatoria B: "<<endl;
+    for(int i=0; i<fil; i++){
+        for(int j=0; j < col; j++){
+        MArestB[i][j]=restRanB(motor2);
+        }
+    }
+    for(int i=0; i<fil; ++i){
+        cout<<"\n| ";
+        for(int j=0; j<col; ++j)
+            cout<<setw(3)<<MArestB[i][j]<<" ";
+        cout<<" |";
+    }
+    cout<<endl;
+    //===========================================
+    int** MArestC = new int*[fil];
+        for(int i=0; i<fil; i++)
+            MArestC[i] = new int[col];
+    cout<<"\nResta de las matrices aleatorias (A-B):\n";
+    for(int i=0; i<fil; i++)
+            for(int j=0; j<col; j++)
+                MArestC[i][j] = MArestA[i][j] - MArestB[i][j];
+    //===========================================
+    imprimirMatriz(MArestC, fil, col);
+}
+
+void byMatriz_Aleatoria(int fil, int col)
+{
+}
+
+void transMatriz_Aleatoria(int fil, int col)
+{
+    unsigned int seedMatrix4=chrono::steady_clock::now().time_since_epoch().count();
+    default_random_engine motor4(seedMatrix4);
+    uniform_int_distribution<int>transRan(0,99);
+
+    int MAtransA[fil][col];
+    //===========================================
+    cout<<"Matriz aleatoria: "<<endl;
+    for(int i=0; i<fil; i++){
+        for(int j=0; j < col; j++){
+        MAtransA[i][j]=transRan(motor4);
+        }
+    }
+    for(int i=0; i<fil; ++i){
+        cout<<"\n| ";
+        for(int j=0; j<col; ++j)
+            cout<<setw(3)<<MAtransA[i][j]<<" ";
+        cout<<" |";
+    }
+    cout<<"\n"<<endl;
+
+    cout<<"Matriz aleatoria transpuesta: "<<endl;
+    for(int i=0; i<fil; ++i){
+        cout<<"\n| ";
+        for(int j=0; j<col; ++j)
+            cout<<setw(3)<<MAtransA[j][i]<<" ";
+        cout<<" |";
+    }
+    cout<<"\n"<<endl;
+}
+
+void imprimirMatriz(int **M, int fil, int col)
+{
+    for(int i=0; i<fil; i++){
+        cout<<"\n| ";
+        for(int j=0; j<col; j++)
+            cout<<setw(3)<<M[i][j]<<" ";
+
+        cout<<" |";
+    }
+    cout<<"\n"<<endl;
 }
